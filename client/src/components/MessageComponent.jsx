1af440cdd50506a5ab21540af90c9216462f9450
PrasanthYT <jprasanth2006@gmall.com>
2024-03-27 15:58:15 +0530

    Created Functionality to the Navigation bar

---
+++ client/src/components/MessageComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function MessageComponent() {
+    return(
+        <>
+        </>
+    )
+}
+
+export default MessageComponent;
---
+++ client/src/components/SearchComponent.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SearchComponent() {
+    return(
+        <>
+        </>
+    )
+}
+
+export default SearchComponent;
