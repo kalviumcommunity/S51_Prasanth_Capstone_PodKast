3ce91d50cb86616966dc15c77e5f4f9557192722
PrasanthYT <jprasanth2006@gmall.com>
2024-04-08 11:38:55 +0530

    Created a resposive design using @media query

--- client/src/components/HomeComponent.jsx
+++ client/src/components/HomeComponent.jsx
@@ -1,29 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-// Components
-import OnboardingComponent from "./OnboardingComponent";
-
 function HomeComponent() {
-
-  const [isNewVisitor, setIsNewVisitor] = useState(false);
-
-  useEffect(() => {
-    const visitedBefore = localStorage.getItem("visitedBefore");
-    if (!visitedBefore) {
-      setIsNewVisitor(true);
-      localStorage.setItem("visitedBefore", true);
-    }
-  }, []);
-
   return (
     <>
-      {isNewVisitor && <OnboardingComponent />}
-      {!isNewVisitor && (
-        <div style={{color: "white"}}>
-          <h1>Welcome to the Home Page!</h1>
-          <p>This is the main content of your home page.</p>
-        </div>
-      )}
+      <div style={{ color: "white" }}>
+        <h1>Welcome to the Home Page!</h1>
+        <p>This is the main content of your home page.</p>
+      </div>
     </>
   );
 }
