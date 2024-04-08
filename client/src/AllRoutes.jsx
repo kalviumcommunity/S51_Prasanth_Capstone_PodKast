f9e722fad429bcf48cce6bd439a163f304325ede
PrasanthYT <jprasanth2006@gmall.com>
2024-04-08 16:28:15 +0530

    Created and designed UI for Sign up page

--- client/src/AllRoutes.jsx
+++ client/src/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import React from "react";
 import Home from "./components/Home";
+import RegisterComponent from "./components/RegisterComponent";
 
 function AllRouter() {
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
+            <Route path="/register" element={<RegisterComponent/>} />
         </Routes>
     )
 }
