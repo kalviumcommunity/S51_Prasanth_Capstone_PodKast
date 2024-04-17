16063c68d189bcd77b6b4994e475604fad9f8d48
Prasanth <jprasanth2006@gmail.com>
2024-04-17 10:04:34 +0530

    Update register.validator.js

--- validators/register.validator.js
+++ validators/register.validator.js
@@ -1,23 +1,19 @@
 const Joi = require('joi');
 
 const publicProfileSchema = Joi.object({
-  bio: Joi.string(),
-  location: Joi.string(),
-  website: Joi.string(),
-  socialMedia: Joi.object({
-    twitter: Joi.string().allow(''),
-    linkedin: Joi.string().allow(''),
-    instagram: Joi.string().allow('')
-  })
-});
+  bio: Joi.string().optional(),
+  location: Joi.string().optional(),
+  website: Joi.string().optional(),
+  socialMedia: socialMediaSchema.optional()
+}).optional();
 
 const userSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
   username: Joi.string().required(),
-  publicUserID: Joi.string(),
-  avatar: Joi.string().required(),
-  publicProfile: publicProfileSchema,
+  publicUserID: Joi.string().optional(),
+  avatar: Joi.string().optional(),
+  publicProfile: publicProfileSchema.optional(),
   password: Joi.string().required()
 });
 
