999673c11eb6f23c0b92c04aec160135bde554dc
PrasanthYT <jprasanth2006@gmall.com>
2024-04-30 11:00:08 +0530

    Added a endpoint for Like and Dislike the post

--- models/users.model.js
+++ models/users.model.js
@@ -1,75 +1,81 @@
-const { ref } = require('joi');
-const mongoose = require('mongoose');
+const { ref } = require("joi");
+const mongoose = require("mongoose");
 
 // Define the schema for publicProfile
 const publicProfileSchema = new mongoose.Schema({
   bio: {
     type: String,
-    required: false
+    required: false,
   },
   location: {
     type: String,
-    required: false
+    required: false,
   },
   website: {
     type: String,
-    required: false
+    required: false,
   },
   socialMedia: {
     type: {
       twitter: {
         type: String,
-        required: false
+        required: false,
       },
       linkedin: {
         type: String,
-        required: false
+        required: false,
       },
       instagram: {
         type: String,
-        required: false
-      }
+        required: false,
+      },
     },
-  }
+  },
 });
 
 // Define the schema for user
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
-    required: true
+    required: true,
   },
   username: {
     type: String,
-    required: true
+    required: true,
   },
   publicUserID: {
     type: String,
-    required: true
+    required: true,
   },
-  avatar:  {
+  avatar: {
     type: String,
-    required: false
+    required: false,
   },
   publicProfile: {
     type: publicProfileSchema,
-    required: false
+    required: false,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
-  posts:[{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'posts'
-  }]
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "posts",
+    },
+  ],
+  likedPosts: {
+    type: [String],
+    default: [],
+  },
 });
 
 // Create the User model
-const User = mongoose.model('users', userSchema);
+const User = mongoose.model("users", userSchema);
 
 module.exports = User;
